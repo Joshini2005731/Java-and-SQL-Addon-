9707a5877ca900deca8a079cc3bd3434ab6c44f0
Joshini2005731 <2322jb18@kgcas.com>
2025-06-12 12:04:05 +0530

    Create HashEmail.java

---
+++ Day6/HashEmail.java
@@ -0,0 +1,19 @@
+package addon;
+
+import java.util.HashSet;
+
+public class studentemail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashSet<String> stuemail=new HashSet();
+		stuemail.add("josh@gmail.com");
+		stuemail.add("jaan4@gmail.com");
+		for(String data:stuemail) {
+			System.out.println(data);
+		
+	}
+
+}
+}
