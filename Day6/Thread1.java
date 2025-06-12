141aa016c57d90137cf06cba29ed1831283e85b1
Joshini2005731 <2322jb18@kgcas.com>
2025-06-12 12:51:43 +0530

    Create Thread1.java

---
+++ Day6/Thread1.java
@@ -0,0 +1,21 @@
+package addon;
+
+public class Main extends Thread {
+
+	public void run() {
+		for(int i=1;i<10;i++) {
+            System.out.println(i);
+		
+          
+		
+	}
+}
+}
+class Execute {
+	 public static void main(String[] args) {
+	        Main m=new Main();
+	       
+	        m.start();
+}
+}
+
